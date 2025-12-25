da6900d4cd837fcb51b9e451be1c2e93b1a2849c
Zach Kelling <z@zeekay.io>
2025-12-25 00:03:24 -0800

    Initial apps v4.2.0 - Calculator, Terminal, Notes, Clock, Calendar, Weather, Photos, Music, Stickies, SystemPreferences

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+/**
+ * @z-os/apps v4.2.0 - zOS Applications
+ */
+
+// Calculator
+export { default as ZCalculator } from './apps/ZCalculator';
+
+// Terminal
+export { default as ZTerminal } from './apps/ZTerminal';
+
+// Notes
+export { default as ZNotes } from './apps/ZNotes';
+
+// TextPad
+export { default as ZTextPad } from './apps/ZTextPad';
+
+// Clock
+export { default as ZClock } from './apps/ZClock';
+
+// Calendar
+export { default as ZCalendar } from './apps/ZCalendar';
+
+// Weather (placeholder - needs API key)
+export { default as ZWeather } from './apps/ZWeather';
+
+// Photos
+export { default as ZPhotos } from './apps/ZPhotos';
+
+// Music
+export { default as ZMusic } from './apps/ZMusic';
+
+// Stickies
+export { default as ZStickies } from './apps/ZStickies';
+
+// System Preferences
+export { default as ZSystemPreferences } from './apps/ZSystemPreferences';
+
+// App Types
+export * from './types';
